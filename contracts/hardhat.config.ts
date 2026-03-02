f108e42940c7f2814d9c95ee2d6f02970f38991f
mujahid002 <0xmujahid@email.com>
2026-03-02 09:20:45 +0530

    added baseSepolia

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,41 @@
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable, defineConfig } from "hardhat/config";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default defineConfig({
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    baseSepolia: {
+      type: "http",
+      chainType: "l1",
+      url: process.env.BASE_SEPOLIA_RPC_URL as string,
+      accounts: [process.env.CRE_PRIVATE_KEY as string],
+    },
+  },
+});
